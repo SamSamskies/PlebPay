ccc20e6f6874adc93042bc9081f14f6638364e5d
Sam Samskies <samsamskies@gmail.com>
2022-01-07 13:14:36 -0600

    feat: display ⚡️ invoice QR code when quote received

--- components/Paywall/Paywall.js
+++ components/Paywall/Paywall.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import QRCode from "qrcode.react";
 import Button from "../Button";
 import createQuote from "../../utils/createQuote";
+import styles from "./Paywall.module.css";
 
 export default function Paywall({ title, amount, currency, invoiceId }) {
+  const [quote, setQuote] = useState();
   const displayAmount = new Intl.NumberFormat("en", {
     style: "currency",
     currency,
@@ -9,15 +13,25 @@ export default function Paywall({ title, amount, currency, invoiceId }) {
     .format(amount)
     .replace(/\.00$/, "");
   const handleClick = async () => {
-    const quote = await createQuote(invoiceId);
-
-    console.log("quote", quote);
+    setQuote(await createQuote(invoiceId));
   };
 
   return (
     <div>
       <h1>{title}</h1>
-      <Button onClick={handleClick}>Enter for {displayAmount}</Button>
+      {!quote && (
+        <Button onClick={handleClick}>Enter for {displayAmount}</Button>
+      )}
+      {quote && (
+        <div className={styles.qrCodeContainer}>
+          <QRCode
+            value={quote.lnInvoice}
+            bgColor="black"
+            fgColor="white"
+            size={192}
+          />
+        </div>
+      )}
     </div>
   );
 }
