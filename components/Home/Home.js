6570d287d822722a92e47fb191b68e7e48aa341e
Sam Samskies <samsamskies@gmail.com>
2022-01-14 04:31:43 -0600

    feat: add a way to get to create page from home page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,3 +1,24 @@
+import { useRouter } from "next/router";
+import styles from "./Home.module.css";
+import Button from "../Button";
+
 export default function Home() {
-  return <h1>Strike Paywall</h1>;
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/create/${e.target.username.value}`);
+  };
+
+  return (
+    <div className={styles.root}>
+      <h1>Strike Paywall</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Strike Username{" "}
+          <input name="username" placeholder="jack" autoFocus required />
+        </label>
+        <Button>Let&apos;s Go!</Button>
+      </form>
+    </div>
+  );
 }
