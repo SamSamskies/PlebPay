0069b3cc8d58c442ae1b434611c805ceb3ef7f3b
Sam Samskies <samsamskies@gmail.com>
2022-01-22 10:32:21 -0600

    refactor: prevent outside requests to fetch invoice endpoint

--- pages/api/invoices/[invoiceId].js
+++ pages/api/invoices/[invoiceId].js
@@ -1,5 +1,9 @@
 import fetchInvoiceById from "../../../utils/strikeApi/fetchInvoiceById";
 
 export default async function handler(req, res) {
-  res.status(200).json(await fetchInvoiceById(req.query.invoiceId));
+  if (req.headers.referer && !req.headers.referer.includes("api")) {
+    res.status(200).json(await fetchInvoiceById(req.query.invoiceId));
+  } else {
+    res.status(403).send("Forbidden");
+  }
 }
