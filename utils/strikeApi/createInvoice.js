fc1eaf9d1225395567ef55560336ac350085d763
Sam Samskies <samsamskies@gmail.com>
2022-02-02 20:50:56 -0600

    feat: set correlation ID to the paywall invoice ID

--- utils/strikeApi/createInvoice.js
+++ utils/strikeApi/createInvoice.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const createInvoice = ({ title, amount, currency, redirectUrl, username }) => {
+const createInvoice = ({
+  invoiceId,
+  title,
+  amount,
+  currency,
+  redirectUrl,
+  username,
+}) => {
   const data = {
+    correlationId: invoiceId,
     description: redirectUrl ? JSON.stringify({ title, redirectUrl }) : title,
     amount: {
       currency,
