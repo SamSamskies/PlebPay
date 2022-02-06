0af14c23e335a3bbe92281174afd7c92c49acade
Sam Samskies <samsamskies@gmail.com>
2022-02-06 17:31:42 -0600

    Revert "feat: set correlation ID to the paywall invoice ID"
    This reverts commit fc1eaf9d1225395567ef55560336ac350085d763.

--- utils/strikeApi/createInvoice.js
+++ utils/strikeApi/createInvoice.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-const createInvoice = ({
-  invoiceId,
-  title,
-  amount,
-  currency,
-  redirectUrl,
-  username,
-}) => {
+const createInvoice = ({ title, amount, currency, redirectUrl, username }) => {
   const data = {
-    correlationId: invoiceId,
     description: redirectUrl ? JSON.stringify({ title, redirectUrl }) : title,
     amount: {
       currency,
