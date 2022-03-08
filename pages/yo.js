223d77eb2c855b0b7aa93d6b61ab41617d44288a
Sam Samskies <samsamskies@gmail.com>
2022-03-08 13:58:46 -0600

    asdf

--- pages/yo.js
+++ pages/yo.js
@@ -1,21 +1,42 @@
+import { Link } from "@chakra-ui/react";
+
 export default function Custom404() {
   return (
     <div>
       <h1>Mainnet</h1>
-      <a href="strike:bitcoin:bc1q8n477r3dnrdpm5cvz8t7w33trg57ndschkhydt">
+
+      <Link
+        href="strike:bitcoin:bc1q8n477r3dnrdpm5cvz8t7w33trg57ndschkhydt"
+        isExternal
+        variant="brand"
+      >
         strike:bitcoin:bc1q8n477r3dnrdpm5cvz8t7w33trg57ndschkhydt
-      </a>
-      <a href="strike:lightning:lnbc1500n1p3z0dpupp5t02jrudxsnrhh0ck5gj0wxnmga6vkvmpu6wx2m7kfnczkxecedsqdpa2fjkzep6ypyx7aeqw3hjqct4w3hk6ct5d93kzmrv0ys82uryv96x2greda6hycqzysxqr23ssp5gu2qqxxxeasqat7kxf2uprusy7yla8zcjjmrfc06degd4hpap2ts9qyyssqhegu8hctx4eg8cnjnjtp05quj6r3q3ga3902wc3ekt76ku2g0murgkjcaqsaczx0upnrc9jhxvea6f7szg9ktd720jy32h89xv3d3rgp7ganr6">
+      </Link>
+      <br />
+      <Link
+        href="strike:lightning:lnbc1500n1p3z0dpupp5t02jrudxsnrhh0ck5gj0wxnmga6vkvmpu6wx2m7kfnczkxecedsqdpa2fjkzep6ypyx7aeqw3hjqct4w3hk6ct5d93kzmrv0ys82uryv96x2greda6hycqzysxqr23ssp5gu2qqxxxeasqat7kxf2uprusy7yla8zcjjmrfc06degd4hpap2ts9qyyssqhegu8hctx4eg8cnjnjtp05quj6r3q3ga3902wc3ekt76ku2g0murgkjcaqsaczx0upnrc9jhxvea6f7szg9ktd720jy32h89xv3d3rgp7ganr6"
+        isExternal
+        variant="brand"
+      >
         strike:lightning:lnbc1500n1p3z0dpupp5t02jrudxsnrhh0ck5gj0wxnmga6vkvmpu6wx2m7kfnczkxecedsqdpa2fjkzep6ypyx7aeqw3hjqct4w3hk6ct5d93kzmrv0ys82uryv96x2greda6hycqzysxqr23ssp5gu2qqxxxeasqat7kxf2uprusy7yla8zcjjmrfc06degd4hpap2ts9qyyssqhegu8hctx4eg8cnjnjtp05quj6r3q3ga3902wc3ekt76ku2g0murgkjcaqsaczx0upnrc9jhxvea6f7szg9ktd720jy32h89xv3d3rgp7ganr6
-      </a>
+      </Link>
 
       <h1>Testnet</h1>
-      <a href="strike:bitcoin:mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt">
+      <Link
+        href="strike:bitcoin:mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt"
+        isExternal
+        variant="brand"
+      >
         strike:bitcoin:mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt
-      </a>
-      <a href="strike:lightning:lntb1500n1p3zygvtpp5grt3qkq0r4xkj8waa3qfaht9z3st6yg26vhd56gvx6v539ctjluqdpa2fjkzep6ypyjq6n4wd6zqen0w4hxggrwv94k2epqwp5kxgr0vcsyy6tvd35k2cqzpgxqr23ssp5g2c855rhpds50pf9uzv3yvgsch79te5f5cwpkljhlx7s7ggfdhas9qyyssqy27zdz3a4xgnyrwzhlqjwk2t54wp5kctllprcgxm2lx9xcssc2kpxztjsfvcadl9cgefftncrhrxyjs30ckgesun6d0frrerkwc9pngqzdvhj7">
+      </Link>
+      <br />
+      <Link
+        href="strike:lightning:lntb1500n1p3zygvtpp5grt3qkq0r4xkj8waa3qfaht9z3st6yg26vhd56gvx6v539ctjluqdpa2fjkzep6ypyjq6n4wd6zqen0w4hxggrwv94k2epqwp5kxgr0vcsyy6tvd35k2cqzpgxqr23ssp5g2c855rhpds50pf9uzv3yvgsch79te5f5cwpkljhlx7s7ggfdhas9qyyssqy27zdz3a4xgnyrwzhlqjwk2t54wp5kctllprcgxm2lx9xcssc2kpxztjsfvcadl9cgefftncrhrxyjs30ckgesun6d0frrerkwc9pngqzdvhj7"
+        isExternal
+        variant="brand"
+      >
         strike:lightning:lntb1500n1p3zygvtpp5grt3qkq0r4xkj8waa3qfaht9z3st6yg26vhd56gvx6v539ctjluqdpa2fjkzep6ypyjq6n4wd6zqen0w4hxggrwv94k2epqwp5kxgr0vcsyy6tvd35k2cqzpgxqr23ssp5g2c855rhpds50pf9uzv3yvgsch79te5f5cwpkljhlx7s7ggfdhas9qyyssqy27zdz3a4xgnyrwzhlqjwk2t54wp5kctllprcgxm2lx9xcssc2kpxztjsfvcadl9cgefftncrhrxyjs30ckgesun6d0frrerkwc9pngqzdvhj7
-      </a>
+      </Link>
     </div>
   );
 }
