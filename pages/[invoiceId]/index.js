a0e5f3e2d0a409243a50a9d7cb9fe9f30931753d
Sam Samskies <samsamskies@gmail.com>
2024-11-03 17:28:36 -0600

    validate invoice ID format
    this is to prevent assholes from trying to hack the app and make unnecessary Strike API calls.

--- pages/[invoiceId]/index.js
+++ pages/[invoiceId]/index.js
@@ -4,35 +4,62 @@ import fetchUserById from "utils/strikeApi/fetchUserById";
 import { getRedirectUrl, getTitle } from "utils/invoice";
 import { createPlebPayRef, createSaltedPaywallId } from "utils/hashing";
 
+function isUuid(uuidString) {
+  const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+  return re.test(uuidString);
+}
+
 export async function getServerSideProps({ query, req }) {
-  const invoice = await fetchInvoiceById(query.invoiceId);
-
-  if (!invoice) {
+  if (!isUuid(query.invoiceId)) {
     return { notFound: true };
   }
 
-  const { amount, receiverId } = invoice;
-  const title = getTitle(invoice);
-  const redirectUrl = getRedirectUrl(invoice);
+  try {
+    const invoice = await fetchInvoiceById(query.invoiceId);
 
-  if (title === null || redirectUrl === null) {
-    return { notFound: true };
+    if (!invoice) {
+      return { notFound: true };
+    }
+
+    const { amount, receiverId } = invoice;
+    const title = getTitle(invoice);
+    const redirectUrl = getRedirectUrl(invoice);
+
+    if (title === null || redirectUrl === null) {
+      return { notFound: true };
+    }
+
+    const { handle } = await fetchUserById(receiverId);
+
+    return {
+      props: {
+        amount: amount.amount,
+        currency: amount.currency,
+        invoiceId: query.invoiceId,
+        title,
+        username: handle,
+        saltedPaywallId: createSaltedPaywallId(query.invoiceId),
+        plebPayRef: createPlebPayRef(
+          query.invoiceId,
+          req.headers["user-agent"]
+        ),
+        isProofOfPlebPay: redirectUrl === "proofofplebpay",
+      },
+    };
+  } catch (error) {
+    if (error?.response?.status === 404) {
+      return { notFound: true };
+    }
+
+    // TODO: display proper error depending on status code
+    return {
+      props: {},
+      redirect: {
+        destination: "/500",
+        permanent: false,
+      },
+    };
   }
-
-  const { handle } = await fetchUserById(receiverId);
-
-  return {
-    props: {
-      amount: amount.amount,
-      currency: amount.currency,
-      invoiceId: query.invoiceId,
-      title,
-      username: handle,
-      saltedPaywallId: createSaltedPaywallId(query.invoiceId),
-      plebPayRef: createPlebPayRef(query.invoiceId, req.headers["user-agent"]),
-      isProofOfPlebPay: redirectUrl === "proofofplebpay",
-    },
-  };
 }
 
 export default Paywall;
